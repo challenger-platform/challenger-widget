c12db36f5dfcce8b5ed23a21c3006b42a8a5f2ea
Tomas Stan <tomas.stan@enga.systems>
2020-07-17 15:33:27 +0300

    More clarificaion works

--- widget.js
+++ widget.js
@@ -11,14 +11,11 @@ function transformChallengeName(value){
 }
 
 var widget = {
-	slidesCount: 0,
+	slidesCount: $('.slide').length, // Count sliding panes
 	currentSlide: 0,
 	user: {},
 
 	init: function () {
-		// Count sliding panes
-		widget.slidesCount = $('.slide').length
-
 		// Try to authenticate user first
 		$.get(challengerServer + "/api/widget/authenticateUser?data=" + encodeURIComponent(encrypted_data), (data) => {
 			if(data.status == 'error'){
@@ -28,8 +25,13 @@ var widget = {
 
 			widget.user = data.user
 
-			widget.loadData()
-			$('.slide:eq(0)').show() // Open first slide
+			$('.user-name').text(widget.user.user_name)
+			$('#intro-block').show() // Open first slide
+
+			widget.showRegistered()
+
+
+			// widget.loadData()
 
 		}).fail(function( jqXHR, textStatus, errorThrown ) {
 			if(jqXHR.status == 401){ // User is now authorized, so we assume he is not registered
@@ -39,47 +41,50 @@ var widget = {
 	},
 
 	loadData: function(){
-		// Load data
+		// Load user points
 		$.get(challengerServer + "/api/widget/getUserPoints", (data) => {
 			$('.show-on-load.unregistered-user').hide(); // User can just get registered so let's hide this block
 
-			$("#points-block").html('You have <span class="points">' + data + '</span> ' + transformPointsName(data) + '!');
-			$("#points").text(data);
-			$('.show-on-load.registered-user').fadeIn(); // Let widget show slides when this block is loaded
+			$("#points-block").html('You have <span class="points">' + data + '</span> ' + transformPointsName(data) + '!')
+		})
 
-			// If user is registered, let's load some more data
-			// Load data
-			$.get(challengerServer + "/api/widget/getUserNotCollectedPoints", (data) => {
-				if(data){
-					$("#not-collected-points-block").addClass('slide').html('Collect <span class="points">' + data + '</span> more ' + transformPointsName(data) + '!');
-				}else{
-					$("#not-collected-points-block").removeClass('slide');
-				}
-				widget.slidesCount = $('.slide').length;
-			})
+		// If user is registered, let's load some more data
+		// Load data
+		$.get(challengerServer + "/api/widget/getUserNotCollectedPoints", (data) => {
+			if(data){
+				$("#not-collected-points-block").addClass('slide').html('Collect <span class="points">' + data + '</span> more ' + transformPointsName(data) + '!');
+			}else{
+				$("#not-collected-points-block").removeClass('slide');
+			}
 
-			// Load data
-			$.get(challengerServer + "/api/widget/getUserChallengesQuantity", (data) => {
-				if(data > 0){
-					$("#available-challenges-block").addClass('slide').html('You have <span class="points">' + data + '</span> ' + transformChallengeName(data));
-				}else{
-					$("#available-challenges-block").removeClass('slide');
-				}
+			widget.slidesCount = $('.slide').length;
+		})
 
-				widget.slidesCount = $('.slide').length;
-			})
+		// Load data
+		$.get(challengerServer + "/api/widget/getUserChallengesQuantity", (data) => {
+			if(data > 0){
+				$("#available-challenges-block").addClass('slide').html('You have <span class="points">' + data + '</span> ' + transformChallengeName(data))
+			}else{
+				$("#available-challenges-block").removeClass('slide')
+			}
+
+			widget.slidesCount = $('.slide').length
 		})
 	},
 
 	showUnregistered: function () {
 		$('.show-on-load.unregistered-user').fadeIn() // show block for unregistered user
 	},
+
+	showRegistered: function () {
+		$('.show-on-load.registered-user').fadeIn() // show block for unregistered user
+	},
 }
 
 $(document).ready(function(){
 	widget.init()
-})
 
-$('#widgetApp').click(function () {
-  window.open(challengerServer + '/login-user-by-external-id?encrypted_data=' + encrypted_data, 'challenger')
+	$('#widgetApp').click(function () {
+	  window.open(challengerServer + '/login-user-by-external-id?encrypted_data=' + encrypted_data, 'challenger')
+	})
 })
